e3fd7532583ed9a61515e7186022ed34d028b3e4
satyaa232323 <attalasatya@gmail.com>
2024-12-31 19:05:22 +0700

    first commit

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::middleware(['clerk'])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    });
+});
